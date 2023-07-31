084df1225fa65d4b2b2da746d68d72791e32b9f8
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-31 16:51:54 +0300

    Create ManagerWidth

---
+++ lib/core/resources/manager_width.dart
@@ -0,0 +1,9 @@
+class ManagerWidth {
+  static const double w2 = 2;
+  static const double w4 = 4;
+  static const double w6 = 6;
+  static const double w8 = 8;
+  static const double w10 = 10;
+  static const double w12 = 12;
+  static const double w100 = 100;
+}
